f122d313268c30d1f7faf543c7ad7715acee6f28
jiho10245 <Pjn7946>
2023-12-27 19:38:38 +0900

    [chore] 데이터 세부 내용 변경

--- datas.js
+++ datas.js
@@ -1,8 +1,8 @@
 const fruitsDatas = [
-    { id: 1, name: "사과", imageUrl: "" },
-    { id: 2, name: "딸기", imageUrl: "" },
-    { id: 3, name: "바나나", imageUrl: "" },
-    { id: 4, name: "포도", imageUrl: "" },
+    { id: 1, name: "사과", description: "제일 먼저 사야함" },
+    { id: 2, name: "딸기", description: "2박스 사기" },
+    { id: 3, name: "바나나", description: "크기 잘 확인하기" },
+    { id: 4, name: "포도", description: "5송이 사기" },
 ];
 
 module.exports = fruitsDatas;
